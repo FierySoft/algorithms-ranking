afb781a8271ab23e8b20bf0aaf82a67449ac5bcd
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-05-31 00:26:09 +0300

    [+] AlgorithmsController : created with db service

---
+++ AlgorithmsRanking/Controllers/AlgorithmsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlgorithmsRanking.Controllers
+{
+    using AlgorithmsRanking.Entities;
+    using AlgorithmsRanking.Services;
+
+    [Route("api/[controller]")]
+    public class AlgorithmsController : Controller
+    {
+        private readonly ResearchRepository _db;
+
+        public AlgorithmsController(ResearchRepository repository)
+        {
+            _db = repository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _db.GetAlgorithmsAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _db.GetAlgorithmAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]Algorithm model)
+        {
+            try
+            {
+                return Ok(await _db.CreateAlgorithmAsync(model));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+        
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Edit([FromRoute]int id, [FromBody]Algorithm model)
+        {
+            try
+            {
+                return Ok(await _db.UpdateAlgorithmAsync(id, model));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+        
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute]int id)
+        {
+            try
+            {
+                await _db.RemoveAlgorithmAsync(id);
+
+                return Ok(new { deleted = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ AlgorithmsRanking/Services/ResearchRepository.Algorithms.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AlgorithmsRanking.Entities;
+
+namespace AlgorithmsRanking.Services
+{
+    public partial class ResearchRepository
+    {
+        public Task<Algorithm[]> GetAlgorithmsAsync()
+        {
+            return _db.Algorithms.ToArrayAsync();
+        }
+
+        public Task<Algorithm> GetAlgorithmAsync(int id)
+        {
+            return _db.Algorithms.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Algorithm> CreateAlgorithmAsync(Algorithm model)
+        {
+            var create = _db.Algorithms.Add(model).Entity;
+
+            await _db.SaveChangesAsync();
+
+            return create;
+        }
+
+        public async Task<Algorithm> UpdateAlgorithmAsync(int id, Algorithm model)
+        {
+            var update = await GetAlgorithmAsync(id);
+
+            _db.Algorithms.Update(update);
+            await _db.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task RemoveAlgorithmAsync(int id)
+        {
+            var remove = await GetAlgorithmAsync(id);
+
+            _db.Algorithms.Remove(remove);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
