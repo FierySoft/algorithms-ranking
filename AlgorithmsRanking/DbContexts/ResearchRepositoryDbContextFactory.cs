13773b97ed8bd47eb36ebbbf8291c31b1b6c111a
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-05-31 00:22:07 +0300

    [+] DbContext : added db context, configured in startup

---
+++ AlgorithmsRanking/DbContexts/ResearchRepositoryDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using AlgorithmsRanking.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AlgorithmsRanking
+{
+    public class ResearchRepositoryDbContextFactory : IDesignTimeDbContextFactory<ResearchRepositoryDbContext>
+    {
+        public ResearchRepositoryDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ResearchRepositoryDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ResearchDb;Trusted_Connection=True;ConnectRetryCount=0");
+
+            return new ResearchRepositoryDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- AlgorithmsRanking/Startup.cs
+++ AlgorithmsRanking/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -9,19 +10,21 @@ namespace AlgorithmsRanking
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<Services.TicketsService>();
-
             services.AddMvc();
+
+            services.AddDbContext<DbContexts.ResearchRepositoryDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -61,7 +64,7 @@ namespace AlgorithmsRanking
 
                 if (env.IsDevelopment())
                 {
-                    spa.Options.StartupTimeout = new TimeSpan(0, 0, 90);
+                    spa.Options.StartupTimeout = new TimeSpan(0, 0, 50);
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
