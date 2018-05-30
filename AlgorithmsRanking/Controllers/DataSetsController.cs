6267ffb4241272e75d6a09712f3c5e490fa3a0ee
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-05-31 00:35:12 +0300

    [+] DataSetsController : created with db service

---
+++ AlgorithmsRanking/Controllers/DataSetsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlgorithmsRanking.Controllers
+{
+    using AlgorithmsRanking.Entities;
+    using AlgorithmsRanking.Services;
+
+    [Route("api/[controller]")]
+    public class DataSetsController : Controller
+    {
+        private readonly ResearchRepository _db;
+
+        public DataSetsController(ResearchRepository repository)
+        {
+            _db = repository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _db.GetDataSetsAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _db.GetDataSetAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]DataSet model)
+        {
+            try
+            {
+                return Ok(await _db.CreateDataSetAsync(model));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Edit([FromRoute]int id, [FromBody]DataSet model)
+        {
+            try
+            {
+                return Ok(await _db.UpdateDataSetAsync(id, model));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute]int id)
+        {
+            try
+            {
+                await _db.RemoveDataSetAsync(id);
+
+                return Ok(new { deleted = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ AlgorithmsRanking/Services/ResearchRepository.DataSets.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AlgorithmsRanking.Entities;
+
+namespace AlgorithmsRanking.Services
+{
+    public partial class ResearchRepository
+    {
+        public Task<DataSet[]> GetDataSetsAsync()
+        {
+            return _db.DataSets.ToArrayAsync();
+        }
+
+        public Task<DataSet> GetDataSetAsync(int id)
+        {
+            return _db.DataSets.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<DataSet> CreateDataSetAsync(DataSet model)
+        {
+            var create = _db.DataSets.Add(model).Entity;
+
+            await _db.SaveChangesAsync();
+
+            return create;
+        }
+
+        public async Task<DataSet> UpdateDataSetAsync(int id, DataSet model)
+        {
+            var update = await GetDataSetAsync(id);
+
+            _db.DataSets.Update(update);
+            await _db.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task RemoveDataSetAsync(int id)
+        {
+            var remove = await GetDataSetAsync(id);
+
+            _db.DataSets.Remove(remove);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
