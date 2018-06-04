cf1def59ddaec303aea785c513a72276d24ed0f7
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-06-04 18:26:49 +0300

    [+] AttachmentsController : added endpoint and service for file uploading to server

---
+++ AlgorithmsRanking/Controllers/AttachmentsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AlgorithmsRanking.Controllers
+{
+    using AlgorithmsRanking.Models;
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class AttachmentsController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public AttachmentsController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult UploadFile()
+        {
+            try
+            {
+                var folderName = "files";
+                var webRootPath = _hostingEnvironment.WebRootPath;
+                var newPath = Path.Combine(webRootPath, folderName);
+                var urlBasePath = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}/{folderName}";
+
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+
+                var urls = new List<string>();
+
+                foreach (var file in Request.Form.Files)
+                {
+                    if (file.Length > 0)
+                    {
+                        string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        string fullPath = Path.Combine(newPath, fileName);
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+
+                        urls.Add($"{urlBasePath}/{fileName}");
+                    }
+                }
+
+                return Ok(new { message = "Загрузка успешно завершена", urls });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ApiError("500", $"Не удалось загрузить файл", ex.Message));
+            }
+        }
+    }
+}
---
+++ AlgorithmsRanking/Services/ResearchRepository.Attachments.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using AlgorithmsRanking.Entities;
+
+namespace AlgorithmsRanking.Services
+{
+    public partial class ResearchRepository
+    {
+        public Task<Attachment[]> GetAttachmentsForDataSetAsync(int dataSetId)
+        {
+            return _db.Attachments.Where(x => x.DataSetId == dataSetId).ToArrayAsync();
+        }
+
+        public async Task<Attachment> CreateAttachmentAsync(Attachment model)
+        {
+            var create = _db.Attachments.Add(model).Entity;
+
+            await _db.SaveChangesAsync();
+
+            return create;
+        }
+
+        public Task CreateAttachmentsAsync(IEnumerable<Attachment> items)
+        {
+            _db.Attachments.AddRange(items);
+
+            return _db.SaveChangesAsync();
+        }
+    }
+}
