b1b1bbfb4376258b997a766715be69fbbb79c4e5
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-06-05 14:01:02 +0300

    [+] SharedModule : added SortableTableDirective, SortableColumnComponent for table sorting by header click

--- AlgorithmsRanking/ClientApp/src/app/shared/shared.module.ts
+++ AlgorithmsRanking/ClientApp/src/app/shared/shared.module.ts
@@ -6,9 +6,10 @@ import { AceEditorModule } from 'ng2-ace-editor';
 import { SpinnerComponent } from './spinner.component';
 import { ErrorComponent } from './error.component';
 import { FileUploadComponent } from './file-upload.component';
+import { PersonCardComponent } from './person-card.component';
 import { FileSizePipe } from './file-size.pipe';
 import { Popover, PopoverContent } from './popover.component';
-import { PersonCardComponent } from './person-card.component';
+import { SortableTableDirective, SortableColumnComponent, SortService } from './sortable-table.directive';
 
 @NgModule({
     imports: [
@@ -20,21 +21,23 @@ import { PersonCardComponent } from './person-card.component';
         SpinnerComponent,
         ErrorComponent,
         FileUploadComponent,
+        PersonCardComponent,
         FileSizePipe,
         Popover, PopoverContent,
-        PersonCardComponent
+        SortableTableDirective, SortableColumnComponent
     ],
     providers: [
-
+        SortService
     ],
     exports: [
         CommonModule,
         SpinnerComponent,
         ErrorComponent,
         FileUploadComponent,
+        PersonCardComponent,
         FileSizePipe,
         Popover, PopoverContent,
-        PersonCardComponent
+        SortableTableDirective, SortableColumnComponent
     ],
     entryComponents: [
 
