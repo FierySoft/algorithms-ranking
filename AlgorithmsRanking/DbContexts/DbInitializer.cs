5bf970a7aa9be85ebabf21976422ca10e791d374
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-06-06 21:43:55 +0300

    [*] DbContext : added initialization of Account table

--- AlgorithmsRanking/DbContexts/DbInitializer.cs
+++ AlgorithmsRanking/DbContexts/DbInitializer.cs
@@ -19,11 +19,27 @@ namespace AlgorithmsRanking.DbContexts
 
             var persons = new Person[]
             {
-                new Person("Иванов", "Иван", "Иванович", "ivanovii@mail.ru", "+79999999999"),
+                new Person("Иванов", "Иван", "Иванович", "ivanovii@mail.ru", "+79991234567"),
                 new Person("Петров", "Петр", "Петрович", "petrovpp@mail.ru", "+79098765432")
             };
 
             context.Persons.AddRange(persons);
+            context.SaveChanges();
+
+
+            // Look for any accounts
+            if (context.Accounts.Any())
+            {
+                return; // DB has been seeded
+            }
+
+            var accounts = new Account[]
+            {
+                new Account(1, "ivanov", "123", "Admin", "/images/dev-user.jpg"),
+                new Account(2, "petrov", "123", "User", "/images/dev-user.jpg")
+            };
+
+            context.Accounts.AddRange(accounts);
             context.SaveChanges();
 
 
--- AlgorithmsRanking/Entities/Account.cs
+++ AlgorithmsRanking/Entities/Account.cs
@@ -16,5 +16,21 @@ namespace AlgorithmsRanking.Entities
         public Person Person { get; set; }
 
         public DateTime RegisteredAt { get; set; }
+
+
+        public Account()
+        {
+
+        }
+
+        public Account(int personId, string userName, string password, string role, string avatar)
+        {
+            PersonId = personId;
+            UserName = userName;
+            Password = password;
+            Role = role;
+            AvatarUri = avatar;
+            RegisteredAt = DateTime.Now;
+        }
     }
 }
